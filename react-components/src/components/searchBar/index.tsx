f57e2e73b63ba5c36cbbe4c6751af0fb08074847
Alisher Gulamov <gulamaier@gmail.com>
2022-09-29 03:36:01 +0500

    feat: implement save input value to LocalStorage

--- react-components/src/components/searchBar/index.tsx
+++ react-components/src/components/searchBar/index.tsx
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import classes from './index.module.scss';
+import { FiSearch } from 'react-icons/fi';
 
 class SearchBar extends Component {
+  state = { value: '' };
+
+  componentDidMount = () => {
+    const value = localStorage.getItem('searchValue');
+    if (value) this.setState({ value });
+  };
+
+  componentWillUnmount = () => localStorage.setItem('searchValue', this.state.value);
+
+  inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ value: e.target.value });
+  };
+
   render() {
     return (
       <div className={classes.search}>
-        <input type="search" placeholder="Search" className={classes.search__input} />
+        <input
+          type="search"
+          placeholder="Search"
+          className={classes.search__input}
+          value={this.state.value}
+          onChange={this.inputHandler}
+        />
+        <button type="submit" className={classes.search__button}>
+          <FiSearch />
+        </button>
       </div>
     );
   }
