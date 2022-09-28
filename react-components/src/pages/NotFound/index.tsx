f39e41c3a03f67bff1611961609be9871f405977
Alisher Gulamov <gulamaier@gmail.com>
2022-09-29 04:09:58 +0500

    fix: change data and style of notFound page

--- react-components/src/pages/NotFound/index.tsx
+++ react-components/src/pages/NotFound/index.tsx
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import classes from './index.module.scss';
 
 class NotFound extends Component {
   render() {
-    return <h1>Sorry, Page Not Found! (404)</h1>;
+    return (
+      <div className={classes.notFound}>
+        <h2>404. That’s an error.</h2>
+        <p>
+          The requested URL was not found on this server.
+          <br />
+          That’s all we know.
+        </p>
+      </div>
+    );
   }
 }
 
