634ef8d85f2dbe1beaadcc8bde912656f96f28f2
Alisher Gulamov <gulamaier@gmail.com>
2022-10-31 02:56:12 +0500

    refactor: rewrite forms page with the hooks

--- src/pages/Forms/index.test.tsx
+++ src/pages/Forms/index.test.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import Forms from './index';
-import { Form } from 'react-router-dom';
-import { ItemInterface, ItemsType } from 'models';
 
 describe('Forms Page', () => {
   test('renders Forms info', () => {
@@ -13,32 +11,4 @@ describe('Forms Page', () => {
     const element = screen.getByTestId('forms-page');
     expect(element).toBeInTheDocument();
   });
-
-  // test('renders Forms infoass', () => {
-  //   interface IPrevState {
-  //     cards: ItemInterface[];
-  //   }
-
-  //   const setState = jest.fn();
-  //   const addCard = (card: ItemInterface) => {
-  //     const newCard = {
-  //       ...card,
-  //       likes: 0,
-  //       views: 1,
-  //     };
-  //     setState((prevState: IPrevState) => {
-  //       const updatedCards = prevState.cards.concat(newCard);
-  //       return { cards: [...updatedCards] };
-  //     });
-  //   };
-
-  //   render(
-  //     <Forms>
-  //       <Form addCard={addCard} />
-  //     </Forms>
-  //   );
-
-  //   const element = screen.getByTestId('form-block');
-  //   expect(element).toBeInTheDocument();
-  // });
 });
